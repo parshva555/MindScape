0ad9d8d21cb743e722449d478d4ac389a446245a
parshva555 <parshvadani@gmail.com>
2024-02-17 11:24:03 +0530

    Added svg to the layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,11 @@
+import SideBar from "@/components/shared/SideBar";
 import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="root">
+      <SideBar/>
+      
       <div className="root-container">
         <div className="wrapper">
             {children}
---
+++ components/shared/SideBar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+const SideBar = () => {
+  return (
+    <aside className='sidebar'>
+        <div className='flex size-full flex-col gap-4'>
+            <Link href='/' className='sidebar-logo'><Image src="/assets/images/logo-text.svg" alt='logo' width={180} height={28}/></Link>
+        </div>
+    </aside>
+  )
+}
+
+export default SideBar
