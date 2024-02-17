9a16dfa759a3b7a7d150d55b8d37e617b70e990d
parshva555 <parshvadani@gmail.com>
2024-02-17 10:48:06 +0530

    Setting Up Clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
-
+import {ClerkProvider} from '@clerk/nextjs'
 const IBMPlex = IBM_Plex_Sans({ subsets: ["latin"], weight:['400','500','600','700'], variable:'--font-ibm-plex'});
 
 export const metadata: Metadata = {
@@ -16,10 +16,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <ClerkProvider appearance={{
+      variables:{colorPrimary: '#624cf5'}
+    }}>
+          <html lang="en">
       <body className={cn("font-IBMPlex antialiased",IBMPlex.variable)}>
         {children}
         </body>
     </html>
+    </ClerkProvider>
   );
 }
